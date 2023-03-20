2dc81fdbc42f9cebea6ed10e6b20217ab56d5d92
Thayane Almeida <thayane.almeida196@gmail.com>
2023-03-19 22:35:04 -0300

    Exercicio 1

--- SQL_II/Exercicio01.sql
+++ SQL_II/Exercicio01.sql
@@ -4,25 +4,13 @@ USE db_generation_game_online;
 
 CREATE TABLE tb_personagens (
 	id bigint auto_increment,
-    nome varchar(255),
-    poder varchar(255),
+    nome varchar(255) not null,
+    poder_ataque int not null,
+    poder_defesa int not null,
     elixir int,
-    tipo varchar(255),
     PRIMARY KEY(id)
     );
-    
-    INSERT INTO tb_personagens (nome, poder, elixir, tipo)
-    VALUES ("mosqueteira", "atirar", 4, "defesa");
-     INSERT INTO tb_personagens (nome, poder, elixir, tipo)
-    VALUES ("valquiria", "atirar", 4, "defesa e ataque");
-     INSERT INTO tb_personagens (nome, poder, elixir, tipo)
-    VALUES ("megacavaleiro", "esmagar", 7, "defesa e ataque");
-    INSERT INTO tb_personagens (nome, poder, elixir, tipo)
-    VALUES ("corredor", "correr", 4, "ataque");
-	INSERT INTO tb_personagens (nome, poder, elixir, tipo)
-    VALUES ("bebê dragão", "fogo", 4, "defesa aerea");
-    
-    
+       
   CREATE TABLE tb_classes (
 	id bigint auto_increment,
     nivel int,
@@ -44,11 +32,26 @@ CREATE TABLE tb_personagens (
     ALTER TABLE tb_personagens ADD CONSTRAINT fk_personagens_classes
     FOREIGN KEY (classe_id) REFERENCES tb_classes(id);
     
-    SELECT * FROM tb_personagens;
+     
+    INSERT INTO tb_personagens (nome, poder_ataque, poder_defesa, elixir,classe_id)
+    VALUES ("mosqueteira", 1000,3000, 4,1);
+     INSERT INTO tb_personagens (nome, poder_ataque, poder_defesa, elixir,classe_id)
+    VALUES ("valquiria", 4000,4000, 4, 2);
+     INSERT INTO tb_personagens (nome, poder_ataque, poder_defesa, elixir,classe_id)
+    VALUES ("megacavaleiro",5000,3000, 7,3);
+    INSERT INTO tb_personagens (nome, poder_ataque, poder_defesa, elixir,classe_id)
+    VALUES ("corredor", 4000,0,4,4);
+	INSERT INTO tb_personagens (nome, poder_ataque, poder_defesa, elixir,classe_id)
+    VALUES ("bebê dragão",1000,3000,4,4);
     
-    SELECT * FROM tb_classes;
+    SELECT * FROM tb_personagens where poder_ataque > 2000;
     
-    UPDATE tb_personagens set classe_id = 1 WHERE id = 1;
-	UPDATE tb_personagens set classe_id = 2 WHERE id = 2;
-	UPDATE tb_personagens set classe_id = 3 WHERE id = 3;
-	UPDATE tb_personagens set classe_id = 4 WHERE id = 4;
+    SELECT * FROM tb_personagens WHERE poder_defesa >= 1000 AND poder_defesa <= 2000;
+    
+    SELECT * FROM tb_personagens WHERE nome LIKE "%c%";
+    
+    SELECT * FROM tb_personagens INNER JOIN tb_classes ON tb_personagens.classe_id = tb_classes.id;
+    
+    SELECT * FROM tb_personagens INNER JOIN tb_classes ON tb_personagens.classe_id = tb_classes.id WHERE tb_classes.dificuldade = "média";
+    
+  
